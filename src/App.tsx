16d5be19112976ee53af6e85d1ec44ec2458ed1e
Gennaro Jacopo Russo <gennarojacopo@libero.it>
2024-05-12 02:41:16 +0200

    Solutions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,156 @@
-import './App.css'
-import { useEffect, useState } from 'react'
-import { Markup } from 'interweave'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Markup } from "interweave";
+import biologia from "../public/biology.json";
+import chimica from "../public/chemistry.json";
+import conoscenze from "../public/knowledge.json";
+import logica from "../public/logic.json";
+import matematica from "../public/math.json";
 
 interface Answer {
-  id: string
-  text: string
+  id: string;
+  text: string;
 }
 
+type Subject =
+  | "biologia"
+  | "chimica"
+  | "competenze di lettura e conoscenze acquisite negli studi"
+  | "fisica e matematica"
+  | "ragionamento logico e problemi";
+
 interface Question {
-  id: number
-  domanda: string
-  argomento: string
-  nro: number
-  risposte: Answer[]
-  branoId: number |null
+  id: number;
+  domanda: string;
+  argomento: Subject;
+  nro: number;
+  risposte: Answer[];
+  branoId: number | null;
 }
 
 type Api = {
-  id: string
-  domande: Question[]
-}
+  id: string;
+  domande: Question[];
+};
 
-const getCategorySlug = (category: Question['argomento']) => {
+const getCategorySlug = (category: Question["argomento"]) => {
   switch (category) {
-    case 'biologia':
-      return 'biologia';
-    case 'chimica':
-      return 'chimica';
-    case 'fisica e matematica':
-      return 'fis-mat';
-    case 'ragionamento logico e problemi':
-      return 'logica';
-  
+    case "biologia":
+      return "biologia";
+    case "chimica":
+      return "chimica";
+    case "fisica e matematica":
+      return "fis-mat";
+    case "ragionamento logico e problemi":
+      return "logica";
+
     default:
       return category;
   }
-}
+};
+
+const getExplanationFile = (category: Question["argomento"]): typeof biologia => {
+  console.log(category);
+  switch (category) {
+    case "biologia":
+      console.log(biologia);
+      return biologia;
+    case "chimica":
+      console.log(chimica);
+      return chimica;
+    case "fisica e matematica":
+      console.log(matematica);
+      return matematica;
+    case "ragionamento logico e problemi":
+      console.log(logica);
+      return logica;
+    default:
+      console.log(conoscenze);
+      return conoscenze;
+  }
+};
+
+// console.log(
+//   "control",
+//   logica.find((obj) => !obj.id)
+// );
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>();
+  const [currentImgSrc, setCurrentImgSrc] = useState<string[]>([]);
+  const [currentPassage, setCurrentPassage] = useState<string>("");
+  const [isAnswerGiven, setIsAnswerGiven] = useState<boolean>(false);
+  const [isQuestionOver, setIsQuestionOver] = useState<boolean>(false);
 
-  const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>()
-  const [currentImgSrc, setCurrentImgSrc] = useState<string[]>([])
-  const [currentPassage, setCurrentPassage] = useState<string>('')
-  const [isAnswerGiven, setIsAnswerGiven] = useState<boolean>(false)
-  const [isQuestionOver, setIsQuestionOver] = useState<boolean>(false)
-
-  const imgId = currentQuestion?.domanda.includes('includegraphics') ? currentQuestion?.domanda.substring(currentQuestion.domanda.indexOf("includegraphics")) : ''
-  console.log({imgId})
+  const imgId = currentQuestion?.domanda.includes("includegraphics")
+    ? currentQuestion?.domanda.substring(currentQuestion.domanda.indexOf("includegraphics"))
+    : "";
+  // console.log({ imgId });
 
   useEffect(() => {
-    setCurrentImgSrc([])
-    setCurrentPassage('')
+    setCurrentImgSrc([]);
+    setCurrentPassage("");
 
-    fetch('https://domande-ap.mur.gov.it/api/v1/prova')
+    fetch("https://domande-ap.mur.gov.it/api/v1/prova")
       .then((res) => {
         return res.json();
       })
       .then((data: Api) => {
-        const currentRecievedQuestions = data.domande
+        const currentRecievedQuestions = data.domande;
 
-        setCurrentQuestion(currentRecievedQuestions[Math.floor(Math.random() * (currentRecievedQuestions.length)) ])
+        setCurrentQuestion(
+          currentRecievedQuestions[Math.floor(Math.random() * currentRecievedQuestions.length)]
+        );
       });
   }, [isQuestionOver]);
 
   useEffect(() => {
     if (!currentQuestion || !imgId) return;
-      const category = getCategorySlug(currentQuestion.argomento)
-      const regex = /\{(.*?)\}/;
-      const imgQuery = imgId.match(regex)
+    const category = getCategorySlug(currentQuestion.argomento);
+    const regex = /\{(.*?)\}/;
+    const imgQuery = imgId.match(regex);
 
-    fetch(`https://domande-ap.mur.gov.it/assets/includeGraphics/${category}/${imgQuery && imgQuery[1]}`)
+    fetch(
+      `https://domande-ap.mur.gov.it/assets/includeGraphics/${category}/${imgQuery && imgQuery[1]}`
+    )
       .then((res) => res.url)
       .then((data: string) => {
         if (!currentImgSrc.includes(data)) {
           setCurrentImgSrc((prev) => [...prev, data]);
         }
-      }).catch((e) => console.error(e.message));
+      })
+      .catch((e) => console.error(e.message));
   }, [currentQuestion?.domanda]);
 
   useEffect(() => {
-    if(!currentQuestion?.branoId) return;
+    if (!currentQuestion?.branoId) return;
 
     fetch(`https://domande-ap.mur.gov.it/api/v1/domanda/brano/${currentQuestion.branoId}`)
       .then((res) => {
         return res.json();
       })
-      .then((data: {id: string, brano: string}) => {
-        setCurrentPassage(data.brano)
+      .then((data: { id: string; brano: string }) => {
+        setCurrentPassage(data.brano);
       });
-  }, [currentQuestion?.branoId])
+  }, [currentQuestion?.branoId]);
 
   const handleAnswerClick = (e: React.MouseEvent<HTMLDivElement>, answer: Answer) => {
-    setIsAnswerGiven(true)
+    setIsAnswerGiven(true);
 
-    if (answer.id !== 'a') {
-      e.currentTarget.classList.add('clickedAnswer'); 
+    if (answer.id !== "a") {
+      e.currentTarget.classList.add("clickedAnswer");
     }
-  }
+  };
 
   const handleRefreshQuestion = () => {
-    const clickedElements: NodeListOf<HTMLElement> = document.querySelectorAll('.clickedAnswer');
-    clickedElements.forEach((element:HTMLElement) => {
-      element.classList.remove('clickedAnswer');
+    const clickedElements: NodeListOf<HTMLElement> = document.querySelectorAll(".clickedAnswer");
+    clickedElements.forEach((element: HTMLElement) => {
+      element.classList.remove("clickedAnswer");
     });
 
     setIsAnswerGiven(false);
     setIsQuestionOver(!isQuestionOver);
-  }
+  };
 
   // console.log('cqa', currentQuestion)
   // console.log('currentImg', currentImgSrc)
@@ -117,35 +161,65 @@ function App() {
 
   // const question = currentQuestion?.domanda.replace('includegraphics', image)
 
-  return currentQuestion?.domanda && <main>
-    <header className='question'>
-      <h1 className='category'>
-        {currentQuestion?.argomento.toLocaleUpperCase() + ' - ' + currentQuestion?.nro}
-      </h1>
+  if (currentQuestion) {
+    console.log(
+      currentQuestion,
+      getExplanationFile(currentQuestion.argomento).find(
+        (question) => question.id === currentQuestion.id
+      )
+    );
+  } else {
+    console.log("wtf");
+  }
 
-      {currentPassage && <div className='brano' >
-        {currentPassage}
-      </div>}
+  return (
+    currentQuestion?.domanda && (
+      <main>
+        <header className="question">
+          <h1 className="category">
+            {currentQuestion?.argomento.toLocaleUpperCase() + " - " + currentQuestion?.nro}
+          </h1>
 
-      <Markup content={currentQuestion?.domanda} />
-    </header>
+          {currentPassage && <div className="brano">{currentPassage}</div>}
 
-    {currentImgSrc?.length > 0 && <picture>
-      <img src={currentImgSrc[0]} alt="img" />
-    </picture>}
+          <Markup content={currentQuestion?.domanda} />
+        </header>
 
-     <article className='answers'>
-      {currentQuestion.risposte.map((risposta, index) => {
-        return <div className={isAnswerGiven ? (risposta.id === 'a' ? 'correct' : ''): ''} key={risposta.id} onClick={(e) => handleAnswerClick(e, risposta)}> <Markup content={index+1 + ') ' + risposta.text} /></div>
-      })}
-    </article>
+        {currentImgSrc?.length > 0 && (
+          <picture>
+            <img src={currentImgSrc[0]} alt="img" />
+          </picture>
+        )}
 
-    <footer>
-      <button className="refresh" onClick={handleRefreshQuestion}>
-        Nuova domanda
-      </button>
-    </footer>
-  </main>
+        <article className="answers">
+          {currentQuestion.risposte.map((risposta, index) => {
+            return (
+              <div
+                className={isAnswerGiven ? (risposta.id === "a" ? "correct" : "") : ""}
+                key={risposta.id}
+                onClick={(e) => handleAnswerClick(e, risposta)}
+              >
+                <Markup content={index + 1 + ") " + risposta.text} />
+              </div>
+            );
+          })}
+        </article>
+
+        <footer>
+          <button className="refresh" onClick={handleRefreshQuestion}>
+            Nuova domanda
+          </button>
+          <p>
+            {currentQuestion.argomento &&
+              isAnswerGiven &&
+              getExplanationFile(currentQuestion.argomento).find(
+                (question) => question.id === currentQuestion.nro
+              )?.spiegazione}
+          </p>
+        </footer>
+      </main>
+    )
+  );
 }
 
 export default App;
